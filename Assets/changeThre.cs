73a310be4f9bb2c3a140a3853c31d6974091c4f2
yujie sun <yujie.sun@unity3d.com>
2019-08-01 15:26:29 +0800

    merge grass shader and dissolving shader

---
+++ Assets/changeThre.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeThre : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Material m;
+    private float threshold = 0;
+    private bool state = false; //false: 0-1, true: 1-0
+    void Start()
+    {
+        m = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!state)
+        {
+            threshold += 0.01f;
+            m.SetFloat("_Threshold", threshold);
+            if (threshold > 0.99f) state = !state;
+        }
+        else
+        {
+            threshold -= 0.01f;
+            m.SetFloat("_Threshold", threshold);
+            if (threshold < 0.01f) state = !state;
+        }
+    }
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void LateUpdate()
+    {
+        Shader.SetGlobalVector("_Position", gameObject.transform.position);
+    }
+
+    void move()
+    {
+        float movex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertical");
+        Vector3 speedvec = new Vector3(movex, 0, movey);
+        //speedvec.Normalize();
+        rb.velocity = speed * speedvec;
+    }
+}
